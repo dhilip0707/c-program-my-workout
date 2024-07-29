396995df46689117020b6e6ecafcea8ea18b9111
dhilip0707 <dhilip0707@gmail.com>
2024-07-29 07:25:56 +0530

    while

---
+++ Loop-programs/2.while.c
@@ -0,0 +1,32 @@
+//2. Write a C program to compute the sum of the first 10 natural numbers.
+
+#include<stdio.h>
+void main()
+{
+int a,c=0;
+printf("The first 10 natural number is:\n");
+while(a<=10)
+{
+c=a+c;
+printf("%d\n",a);
+a++;
+}
+printf("The Sum is :%d\n",c);
+}
+
+
+/*output
+The first 10 natural number is:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+The Sum is :55
+*/
