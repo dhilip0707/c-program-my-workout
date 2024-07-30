db251e4461e1c12a199c49cc58e6dde592df0794
dhilip0707 <dhilip0707@gmail.com>
2024-07-30 18:47:57 +0530

    pattern

---
+++ Nested loop/sample.pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n, i, j;
+    printf("Input the row value");
+    scanf("%d", &n);
+    for (i = n; i >= 1; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+    printf("\n");
+    }
+    
+}
