da2e11b641892b1d025c610f4d183eb47ea7f26f
dhilip0707 <dhilip0707@gmail.com>
2024-08-05 11:30:06 +0530

    sum of series

--- Loop-programs/23.sum of series.c
+++ Loop-programs/23.sum of series.c
@@ -1,10 +1,10 @@
 // 23. Write a program in C to find the sum of the series [x - x^3 + x^5 + ......].
 
-/*#include <stdio.h>
+#include <stdio.h>
 void main()
 {
     int i,x, n;
-    float  a= 1,sum = 1;
+    float a=1,sum = 1;
     printf("Input the value:");
     scanf("%d", &x);
     printf("Input number of terms:");
@@ -12,26 +12,6 @@ void main()
     for (i = 1; i < n; i++)
     {
         a = a * x /i;
-        sum = sum + a;
-    }
-    printf("The sum is :%f", sum);
-}
-*/
-
-
-#include <stdio.h>
-void main()
-{
-    int i,x,k,n;
-    float  a= 1,sum = 1;
-    printf("Input the value:");
-    scanf("%d", &x);
-    printf("Input number of terms:");
-    scanf("%d", &n);
-    for (i = 1; i <=n; i++)
-    {
-        k=(2*i);
-        a = x*x*x/k;
         sum = sum + a;
     }
     printf("The sum is :%f", sum);
