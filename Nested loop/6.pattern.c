fe01b938badd13317c9a27082597df7e91005906
dhilip0707 <dhilip0707@gmail.com>
2024-08-07 22:32:53 +0530

    pattern

--- Nested loop/6.pattern.c
+++ Nested loop/6.pattern.c
@@ -4,7 +4,7 @@ void main()
     int i, j, n;
     printf("Input the number of rows:");
     scanf("%d", &n);
-    for (i = i; i <= n; i++)
+    for (i = 1; i <= n; i++)
     {
         for (j = 1; j <= i; j++)
         {
