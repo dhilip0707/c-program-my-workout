8298de6b95550c9a2a4b26d959e71683e5cf1bb1
dhilip0707 <dhilip0707@gmail.com>
2024-08-23 14:21:24 +0530

    natural number and  their sum

--- Loop-programs/3.do-while.c
+++ Loop-programs/3.do-while.c
@@ -1,18 +1,19 @@
-//3. Write a C program to display n terms of natural numbers and their sum.
+// 3. Write a C program to display n terms of natural numbers and their sum.
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int a=1,c=0,n;
-printf("Input the number :");
-scanf("%d",&n);
-printf("The first %d natural number is :",n);
-do{
-   c=a+c;
-   printf("%d",a);
-   a++;
-   }while(a<=n);
-printf("\nThe Sum of Natural Number upto %d terms :%d\n",n,c);
+   int a = 1, c = 0, n;
+   printf("Input the number :");
+   scanf("%d", &n);
+   printf("The first %d natural number is :", n);
+   do
+   {
+      c = a + c;
+      printf("%d", a);
+      a++;
+   } while (a <= n);
+   printf("\nThe Sum of Natural Number upto %d terms :%d\n", n, c);
 }
 
 /*
@@ -27,4 +28,3 @@ The first 7 natural number is :1
 7
 The Sum of Natural Number upto 7 terms :28
 */
-
