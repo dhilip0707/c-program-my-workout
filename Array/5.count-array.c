aec96d00740103a9de3c9a74f517713f9c114098
dhilip0707 <dhilip0707@gmail.com>
2024-08-29 07:41:38 +0530

    counting the elements

---
+++ Array/5.count-array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, count=0, n;
+    printf("Input the number of elements to be stored in the array :");
+    scanf("%d", &n);
+    int array[n];
+    //int array[] = {4,5,2,2,2};
+    //n = 5;
+     printf("Input %d elements in the array :",n);
+     for (i = 0; i < n; i++)
+      {
+          printf("\nelement-%d :", i);
+          scanf("%d", &array[i]);
+      } 
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (array[j] != array[i])
+            {
+                printf("if %d, %d\n",j,i);
+                count++;
+                break;
+            }
+        }
+    }
+    printf("Total number of duplicate elements found in the array is :%d\n", count);
+}
