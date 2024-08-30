9348f6ee99f8486e443319fc354e9bca53941535
dhilip0707 <dhilip0707@gmail.com>
2024-08-30 08:24:02 +0530

    greater-or-equal

--- Conditional-Statement/6.else-if.c
+++ Conditional-Statement/6.else-if.c
@@ -1,27 +1,22 @@
 //6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.
 
-#include<stdio.h>
-int main()
+#include <stdio.h>
+void main()
 {
-int x;
-printf("Enter a x:");
-scanf("%d",&x);
-if(x%8!=0){
-  if(x%10==0){
-    if(x%50==0){
-    printf("stu");
-}    
-else{
-	printf("x is zero");
+  int m, n;
+  printf("Input the  value of m :");
+  scanf("%d", &m);
+  if (m != 0)
+  {
+    if (m > 0)
+      n = 1;
+    else
+      n = -1;
+  }
+  else
+    n = 0;
+  printf("The value of n = %d \n", n);
 }
-}    
-else{
-	printf("x is zeo");
-}
-	return 0;
-}
-}
-
 /*
 output
 Enter the value :-5
