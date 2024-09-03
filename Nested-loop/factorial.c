2ac00d321bf5593ddc4455562298242d151d0318
dhilip0707 <dhilip0707@gmail.com>
2024-09-03 09:20:10 +0530

    factorial

--- Nested-loop/factorial.c
+++ Nested-loop/factorial.c
@@ -4,10 +4,10 @@ void main()
     int i = 1, j = 1, n;
     printf("Input the number:");
     scanf("%d", &n);
-    do
+   while (i <= n)
     {
         j = j * i;
         i++;
-    } while (i <= n);
+    } 
     printf("The factorial of %d is :%d\n", n, j);
 }
