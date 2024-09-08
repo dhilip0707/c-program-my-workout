7aba1ab062c5c0a992a962a1a62bca4733c0bda7
dhilip0707 <dhilip0707@gmail.com>
2024-09-08 18:50:51 +0530

    angle

--- Conditional-Statement/15.triangle.c
+++ Conditional-Statement/15.triangle.c
@@ -1,19 +1,21 @@
-//15. Write a C program to check whether a triangle can be formed with the given values for the angles.
+// 15. Write a C program to check whether a triangle can be formed with the given values for the angles.
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int angle1,angle2,angle3,angle;
-printf("Enter the value :");
-scanf("%d%d%d",&angle1,&angle2,&angle3);
-angle=angle1+angle2+angle3;
-if(angle==180){
-	printf("The triangle is valid.\n");
-}
-else{
-	printf("The triangle is invalid.\n");
-}
-return 0;
+	int angle1, angle2, angle3, angle;
+	printf("Enter the value :");
+	scanf("%d%d%d", &angle1, &angle2, &angle3);
+	angle = angle1 + angle2 + angle3;
+	if (angle == 180)
+	{
+		printf("The triangle is valid.\n");
+	}
+	else
+	{
+		printf("The triangle is invalid.\n");
+	}
+	return 0;
 }
 
 /*
