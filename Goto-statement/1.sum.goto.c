3dece8c16130eea53ec1bd19388f1ec887e1eada
dhilip0707 <dhilip0707@gmail.com>
2024-09-08 20:19:00 +0530

    goto

--- Goto-statement/1.sum.goto.c
+++ Goto-statement/1.sum.goto.c
@@ -4,19 +4,22 @@
 #include<stdio.h>
 void main()
 {
-int a=1,Sum=0;
+int a=1,Sum=0,n;
+printf("Enter the input:");
+scanf("%d",&n);
 printf("Natural numbers are:");
 Hello:
 printf("%d ",a);
 Sum=a+Sum;
 a++;
-if(a<=20){
+if(a<=n){
 goto Hello;
 }
 printf("\nSum is:%d\n",Sum);
 }
 
-/*output
+/*
+Output:
 Natural numbers are:1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
 Sum is:210
 */
