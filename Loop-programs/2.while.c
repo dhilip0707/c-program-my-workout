12a84fe7bbf7749daf11a2a2fc9661f9cfda8e22
dhilip0707 <dhilip0707@gmail.com>
2024-10-01 07:22:18 +0530

    loop

--- Loop-programs/2.while.c
+++ Loop-programs/2.while.c
@@ -3,30 +3,23 @@
 #include <stdio.h>
 void main()
 {
-    int a, c = 0;
-    printf("The first 10 natural number is:\n");
-    while (a <= 10)
+    int i = 1, sum = 0, n;
+    printf("Enter the input:");
+    scanf("%d", &n);
+    printf("Natural numbers are :");
+    while (i <= n)
     {
 
-        printf("%d", a);
-        c = a + c;
-        a++;
+        printf("%d ", i);
+        sum = i + sum;
+        i++;
     }
-    printf("\nThe Sum is :%d\n", c);
+    printf("\nThe Sum is :%d\n", sum);
 }
 
-/*output
-The first 10 natural number is:
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
+/*
+Output
+Enter the input:10
+Natural numbers are :1 2 3 4 5 6 7 8 9 10
 The Sum is :55
 */
