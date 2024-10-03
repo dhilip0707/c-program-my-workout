3cd1e7c2aafc34dd689836e4f85f1b583f46022c
dhilip0707 <dhilip0707@gmail.com>
2024-10-03 08:28:28 +0530

    Loop

--- Loop-programs/5.cube.while.c
+++ Loop-programs/5.cube.while.c
@@ -3,25 +3,23 @@
 #include<stdio.h>
 void main()
 {
-int a,b,cube;
-printf("Input the number of terms:\n");
-scanf("%d",&b);
-while(a<=b)
+int i,terms,cube;
+printf("Input the number of terms:");
+scanf("%d",&terms);
+while(i<=terms)
 {
-cube=a*a*a;
-printf("Number is :%d and cube of the %d is:%d\n",a,a,cube);
-a++;
+cube=i*i*i;
+printf("Number is :%d and cube of the %d is:%d\n",i,i,cube);
+i++;
 }
 }
 
-/*output
-Input the number of terms:
-5
-Number is :0 and cube of the 0 is:0
-Number is :1 and cube of the 1 is:1
-Number is :2 and cube of the 2 is:8
-Number is :3 and cube of the 3 is:27
-Number is :4 and cube of the 4 is:64
-Number is :5 and cube of the 5 is:125
+// // Output :
+// Input the number of terms:5
+// Number is :0 and cube of the 0 is:0
+// Number is :1 and cube of the 1 is:1
+// Number is :2 and cube of the 2 is:8
+// Number is :3 and cube of the 3 is:27
+// Number is :4 and cube of the 4 is:64
+// Number is :5 and cube of the 5 is:125
 
-*/
