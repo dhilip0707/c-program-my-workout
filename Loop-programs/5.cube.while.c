64de87316f35200f2958ee5d874e4f823ae6102f
dhilip0707 <dhilip0707@gmail.com>
2024-10-03 08:29:27 +0530

    loop

--- Loop-programs/5.cube.while.c
+++ Loop-programs/5.cube.while.c
@@ -1,20 +1,20 @@
-//5. Write a C program to display the cube of the number up to an integer.
+// 5. Write a C program to display the cube of the number up to an integer.
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-int i,terms,cube;
-printf("Input the number of terms:");
-scanf("%d",&terms);
-while(i<=terms)
-{
-cube=i*i*i;
-printf("Number is :%d and cube of the %d is:%d\n",i,i,cube);
-i++;
-}
+    int i, terms, cube;
+    printf("Input the number of terms:");
+    scanf("%d", &terms);
+    while (i <= terms)
+    {
+        cube = i * i * i;
+        printf("Number is :%d and cube of the %d is:%d\n", i, i, cube);
+        i++;
+    }
 }
 
-// // Output :
+// Output :
 // Input the number of terms:5
 // Number is :0 and cube of the 0 is:0
 // Number is :1 and cube of the 1 is:1
@@ -22,4 +22,3 @@ i++;
 // Number is :3 and cube of the 3 is:27
 // Number is :4 and cube of the 4 is:64
 // Number is :5 and cube of the 5 is:125
-
