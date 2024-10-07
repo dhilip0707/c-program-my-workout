a58771ae01aa8f68f69efeeda61c41b7f757eedf
dhilip0707 <dhilip0707@gmail.com>
2024-10-07 07:53:37 +0530

    Nested

--- Nested-loop/9.pattern.c
+++ Nested-loop/9.pattern.c
@@ -1,15 +1,25 @@
+// 9. Write a program in C to display a pattern like a right angle triangle using an asterisk. (to use for loop)
+
 #include <stdio.h>
 void main()
 {
   int n;
   printf("The pattern like:");
   scanf("%d", &n);
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
-    for (int j = 1; j <= i; j++)
+    for (int j = 0; j <=i; j++)
     {
       printf("*");
     }
     printf("\n");
   }
 }
+
+
+// Output :
+// The pattern like:4
+// *
+// **
+// ***
+// ****
