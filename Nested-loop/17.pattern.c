f2358df00cab70c34d62e8fe19f9a52a585c358b
dhilip0707 <dhilip0707@gmail.com>
2024-10-12 09:02:58 +0530

    pattern

--- Nested-loop/17.pattern.c
+++ Nested-loop/17.pattern.c
@@ -1,8 +1,12 @@
+//17. Write a C program to make such a pattern like a pyramid with a number which will repeat the number in the same row.
+
 #include <stdio.h>
 void main()
 {
-  int i, j, k = 4;
-  for (int i = 1; i <= 4; i++)
+  int i, j, input;
+  printf("Enter the input :");
+  scanf("%d",&input);
+  for (int i = 1; i <= input; i++)
   {
     for (int j = 1; j <= i; j++)
     {
@@ -12,10 +16,9 @@ void main()
   }
 }
 
-/*
-Output:
-1
-22
-333
-4444
-*/
+// Output:
+// Enter the input :4
+// 1
+// 22
+// 333
+// 4444
