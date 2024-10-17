3f7d97d091accc26d48fe3b0ca6d4b74de48bdb7
dhilip0707 <dhilip0707@gmail.com>
2024-10-17 07:37:00 +0530

    sum-of-series

--- Loop-programs/21.nested.c
+++ Loop-programs/21.nested.c
@@ -1,20 +1,20 @@
-//21.Write a program in C to display the sum of the series [ 9 + 99 + 999 + 9999 ...]
+// 21.Write a program in C to display the sum of the series [ 9 + 99 + 999 + 9999 ...]
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int i,n,sum=0,l=9;
+    int i, n, sum = 0, L = 9;
     printf("Input the number of terms:");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-        sum=sum+l;
-        printf("%d ",l);
-        l=l*10+9;
+        sum = sum + L;
+        printf("%d ", L);
+        L = L * 10 + 9;
     }
-    printf("\nThe sum of the series =%d\n",sum);
+    printf("\nThe sum of the series =%d\n", sum);
 }
-/*output
-Input the number of terms:5
-9 99 999 9999 99999 
-The sum of the series =111105*/
+// Output :
+// Input the number of terms:5
+// 9 99 999 9999 99999
+// The sum of the series =111105
