6d1849d8e068ca47536c037ea5ca5a8883c56936
dhilip0707 <dhilip0707@gmail.com>
2024-10-19 17:20:02 +0530

    string

--- Strings/40.anagram.c
+++ Strings/40.anagram.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
+#include<string.h>
 void main()
 {
-    char str1[200], str2[200];
-    int i, j, flag= 0, flag1 = 0, count = 0;
+    char str1[200], str2[200],str3[100],str4[100] ;
+    int i, j, k,n,m,flag = 0, flag1 = 0, count = 0;
     printf("Input the first string:");
     scanf("%s", str1);
     printf("Input the second string:");
     scanf("%s", str2);
-    for (i = 0; str1[i] != '\0'; i++)
+   
+    n=strlen(str1);
+    m=strlen(str2);
+    strcpy(str3,str1);
+    strcpy(str4,str2);
+    if (n != m)
     {
-        flag++;
+        printf("%s and %s are not an Anagram.\n", str3, str4);
     }
-    for (j = 0; str2[j] != '\0'; j++)
-    {
-        flag1++;
-    }
-    for (i = 0; str1[i] != '\0'; i++)
-    {
-        for (j = 0; str2[j] != '\0'; j++)
-        {
-            if (str1[i] == str2[j])
-            {
-                count++;
-                break;
-            }
-        }
-    }
-    if ((flag == count) && (flag1 == count))
-    {
-        printf("%s and %s are Anagram.\n",str1,str2);
-    }
+
     else
     {
-        printf("%s and %s are not an Anagram.\n",str1,str2);
+        for (i = 0; str1[i] != '\0'; i++)
+        {
+            for (j = i + 1; str1[j] != '\0'; j++)
+            {
+                if (str1[i] > str1[j])
+                {
+                    k = str1[i];
+                    str1[i] = str1[j];
+                    str1[j] = k;
+                }
+            }
+        }
+        printf("first:%s", str1);
+        for (i = 0; str2[i] != '\0'; i++)
+        {
+            for (j = i + 1; str2[j] != '\0'; j++)
+            {
+                if (str2[i] > str2[j])
+                {
+                    k = str2[i];
+                    str2[i] = str2[j];
+                    str2[j] = k;
+                }
+            }
+        }
+        printf("\nsecond:%s", str2);
+        for (i = 0; i < n; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            printf("%s and %s is an Anagram.\n", str3, str4);
+        }
+        else
+        {
+            printf("%s and %s are not an Anagram.\n", str3, str4);
+        }
     }
 }
 
