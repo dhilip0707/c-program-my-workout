081545681ecf8e5b9fec2bdb02986a4956fe5753
dhilip0707 <dhilip0707@gmail.com>
2024-10-19 08:43:43 +0530

    sum-of-series

--- Loop-programs/24.sum.of.series.c
+++ Loop-programs/24.sum.of.series.c
@@ -1,36 +1,33 @@
-//24. Write a program in C to find the sum of the series [ x - x^3 + x^5 + ......].
+// 24. Write a program in C to find the sum of the series [ x - x^3 + x^5 + ......].
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int i,n;
-    int x,sum=0,a=1;
-    printf("input the value of x:");
-    scanf("%d",&x);
-    printf("Input the number of %d terms:",n);
-    scanf("%d",&n);
-    a=x; 
-    sum=x;
-    printf("%d\n",a);
-    for(i=1;i<n;i++)
+    int i, n;
+    int x, sum = 0, a = 1;
+    printf("Input the value of x:");
+    scanf("%d", &x);
+    printf("Input the number of %d terms:", n);
+    scanf("%d", &n);
+    a = x;
+    sum = x;
+    printf("%d\n", a);
+    for (i = 1; i < n; i++)
     {
-        
-        a=-a*x*x;
-        sum=sum+a;
-        printf("%d\n",a);
-        
+
+        a = -a * x * x;
+        sum = sum + a;
+        printf("%d\n", a);
     }
-    printf("the sum is:%d",sum);
+    printf("The Sum is:%d\n", sum);
 }
 
-
-/*out
-input the value of x:2
-Input the number of 0 terms:5
-2
--8
-32
--128
-512
-the sum is:410
-*/
+// Output : 
+// Input the value of x:2
+// Input the number of 0 terms:5
+// 2
+// -8
+// 32
+// -128
+// 512
+// The Sum is:410
