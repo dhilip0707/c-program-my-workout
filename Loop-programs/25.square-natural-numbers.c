116346528246f4582dbad4f6ff712ba12c8cb6a7
dhilip0707 <dhilip0707@gmail.com>
2024-10-21 12:23:47 +0530

    Loop

--- Loop-programs/25.square-natural-numbers.c
+++ Loop-programs/25.square-natural-numbers.c
@@ -1,25 +1,25 @@
-//25. Write a C program that displays the n terms of square natural numbers and their sum.
+// 25. Write a C program that displays the n terms of square natural numbers and their sum.
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int i,n,sum=0;
+    int i, n, sum = 0;
     printf("Input the number of terms:");
-    scanf("%d",&n);
-    printf("The square natural upto %d terms are :",n);
-    for ( i = 1; i <=n; i++)
+    scanf("%d", &n);
+    printf("The square natural upto %d terms are :", n);
+    for (i = 1; i <= n; i++)
     {
-        printf("%d+ ",i*i);
-        sum=sum+i*i;
-        
+        printf("%d", i * i);
+        sum = sum + i * i;
+        if (i < n)
+        {
+            printf("+");
+        }
     }
-    printf("\nThe Sum of Square Natural Number upto %d terms =%d\n",n,sum);
-
+    printf("\nThe Sum of Square Natural Number upto %d terms = %d\n", n, sum);
 }
 
-/*
-output
-Input the number of terms:5
-The square natural upto 5 terms are :1  4  9  16  25  
-The Sum of Square Natural Number upto 5 terms =55
-*/
+// Output:
+// Input the number of terms:5
+// The square natural upto 5 terms are :1+4+9+16+25
+// The Sum of Square Natural Number upto 5 terms = 55
