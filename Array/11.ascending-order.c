97f9353fd585e314fccee184ee2c1e9edd8d90f6
dhilip0707 <dhilip0707@gmail.com>
2024-10-21 12:32:20 +0530

    array

--- Array/11.ascending-order.c
+++ Array/11.ascending-order.c
@@ -1,39 +1,36 @@
-//11. Write a program in C to sort elements of an array in ascending order.
+// 11. Write a program in C to sort elements of an array in ascending order.
 
-#include<stdio.h>
-void main ()
+#include <stdio.h>
+void main()
 {
-    int i,j,n,swap;
+    int i, j, n, swap;
     printf("Input the size of array:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int ascending[n];
-    printf("Input %d elements in the array:\n",n);
-    for(i=0;i<n;i++)
+    printf("Input %d elements in the array:\n", n);
+    for (i = 0; i < n; i++)
     {
-        printf("elements-%d:",i);
-        scanf("%d",&ascending[i]);
+        printf("elements-%d:", i);
+        scanf("%d", &ascending[i]);
     }
-    for ( i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for ( j = i+1; j < n; j++)
+        for (j = i + 1; j < n; j++)
         {
-            if (ascending[i]>ascending[j])
+            if (ascending[i] > ascending[j])
             {
-                swap=ascending[i];
-                ascending[i]=ascending[j];
-                ascending[j]=swap;
+                swap = ascending[i];
+                ascending[i] = ascending[j];
+                ascending[j] = swap;
             }
-            
         }
-        
     }
     printf("elements of array in sorted ascending order is :");
     for (i = 0; i < n; i++)
     {
-        printf("%d ",ascending[i]);
+        printf("%d ", ascending[i]);
     }
     printf("\n");
-    
 }
 
 /*
