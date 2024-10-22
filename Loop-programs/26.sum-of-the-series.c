c41311b723cbd0983663d6ba08e752c58029a191
dhilip0707 <dhilip0707@gmail.com>
2024-10-22 09:19:54 +0530

    sum-of-series

--- Loop-programs/26.sum-of-the-series.c
+++ Loop-programs/26.sum-of-the-series.c
@@ -1,21 +1,21 @@
 // 26.Write a program in C to find the sum of the series 1 +11 + 111 + 1111 + .. n terms.
+
 #include <stdio.h>
 void main()
 {
-    int i, sum = 0, n, l = 1;
+    int i, sum = 0, input, sum1 = 1;
     printf("Input the number of terms:");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    scanf("%d", &input);
+    for (i = 1; i <= input; i++)
     {
-        sum = sum + l;
-        printf("%d+", l);
-        l = l * 10 + 1;
+        sum = sum + sum1;
+        printf("%d+", sum1);
+        sum1 = sum1 * 10 + 1;
     }
-    printf("\bThe sum is :%d\n", sum);
+    printf("\b \nThe sum is :%d\n", sum);
 }
 
-/*Output:
-Input the number of terms:5
-1+11+111+1111+11111+
-The sum is :12345
- */
+// Output:
+// Input the number of terms:5
+// 1+11+111+1111+11111
+// The sum is :12345
