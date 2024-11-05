327c9c7a39581b31dbbd68df3f768f608b7a3602
dhilip0707 <dhilip0707@gmail.com>
2024-11-05 08:00:13 +0530

    pattern

--- Nested-loop/pascal-triangle.c
+++ Nested-loop/pascal-triangle.c
@@ -1,40 +1,37 @@
-// pascal's triangle
-#include<stdio.h>
+// Write a C program to display Pascal's triangle.
+
+#include <stdio.h>
 void main()
 {
-    int i,j,n,k,l,m=1;
+    int i, j, input, k, L, m = 1;
     printf("Enter the number:");
-    scanf("%d",&n);
-    for ( i = 0; i < n; i++)
+    scanf("%d", &input);
+    for (i = 0; i < input; i++)
     {
-        for ( j = 0; j < n-i; j++)
+        for (j = 0; j < input - i; j++)
         {
             printf(" ");
         }
-        for ( l = 0; l <=i; l++)
+        for (L = 0; L <= i; L++)
         {
-            if(i==0||l==0)
+            if (i == 0 || L == 0)
             {
-                k=1;
+                k = 1;
             }
             else
             {
-                k=k*(i-l+m)/l;
+                k = k * (i - L + m) / L;
             }
-            printf("%d ",k);
-
+            printf("%d ", k);
         }
         printf("\n");
     }
-    
 }
 
-/*
-Output:
-Enter the number:5
-     1 
-    1 1 
-   1 2 1 
-  1 3 3 1 
- 1 4 6 4 1
- */
+// Output:
+// Enter the number:5
+//      1
+//     1 1
+//    1 2 1
+//   1 3 3 1
+//  1 4 6 4 1
