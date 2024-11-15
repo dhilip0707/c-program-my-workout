4134fdd7b49617332afbeeb27b585e904856a0e9
dhilip0707 <dhilip0707@gmail.com>
2024-11-15 17:51:55 +0530

    binary to octal

--- Nested-loop/binary-octal.c
+++ Nested-loop/binary-octal.c
@@ -1,12 +1,14 @@
+//53.Write a C program to convert a binary number to octal.
+
 #include<stdio.h>
 int binary_to_octal(int);
 void main()
 {
     int binary,octal,sum;
-    printf("Input the binary value:");
+    printf("Input the binary number:");
     scanf("%d",&binary);
     sum=binary_to_octal(binary);
-    printf("The Octal number is : %d\n",sum);
+    printf("The equivalent  Octal number : %d\n",sum);
 }
 int binary_to_octal(int binary)
 {
@@ -29,3 +31,7 @@ int binary_to_octal(int binary)
     }
     return sum;
 }
+
+// Output:
+// Input the binary number:1001
+// The equivalent  Octal number : 11
