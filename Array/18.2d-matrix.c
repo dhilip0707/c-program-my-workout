2d12bfc315e3b4474d92eb264ab6d4a94b900bca
dhilip0707 <dhilip0707@gmail.com>
2024-12-09 08:00:10 +0530

    array

--- Array/18.2d-matrix.c
+++ Array/18.2d-matrix.c
@@ -26,19 +26,18 @@ void main()
    }
 }
 
-/*Output:
-Input the rows and columns in the matrix:3 3
-element - [0],[0]:1
-element - [0],[1]:2
-element - [0],[2]:3
-element - [1],[0]:4
-element - [1],[1]:5
-element - [1],[2]:6
-element - [2],[0]:7
-element - [2],[1]:8
-element - [2],[2]:9
-The Matrix:
-1 2 3
-4 5 6
-7 8 9
-*/
+// Output:
+// Input the rows and columns in the matrix:3 3
+// element - [0],[0]:1
+// element - [0],[1]:2
+// element - [0],[2]:3
+// element - [1],[0]:4
+// element - [1],[1]:5
+// element - [1],[2]:6
+// element - [2],[0]:7
+// element - [2],[1]:8
+// element - [2],[2]:9
+// The Matrix:
+// 1 2 3
+// 4 5 6
+// 7 8 9
