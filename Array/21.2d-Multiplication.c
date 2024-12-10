e69928dee004fa3a84d551a61af1e3f8c9c6b418
dhilip0707 <dhilip0707@gmail.com>
2024-12-10 07:20:19 +0530

    multiplication

--- Array/21.2d-Multiplication.c
+++ Array/21.2d-Multiplication.c
@@ -67,30 +67,29 @@ void main()
     }
 }
 
-/*Output:
-Input the rows and columns of the matrix :2 2
+// Output:
+// Input the rows and columns of the matrix :2 2
 
-Input elements in the first matrix:
-element-[0],[0] :1
-element-[0],[1] :2
-element-[1],[0] :3
-element-[1],[1] :4
+// Input elements in the first matrix:
+// element-[0],[0] :1
+// element-[0],[1] :2
+// element-[1],[0] :3
+// element-[1],[1] :4
 
-Input elements in the second matrix:
-element-[0],[0] :5
-element-[0],[1] :6
-element-[1],[0] :7
-element-[1],[1] :8
+// Input elements in the second matrix:
+// element-[0],[0] :5
+// element-[0],[1] :6
+// element-[1],[0] :7
+// element-[1],[1] :8
 
-The First matrix is :
-1 2
-3 4
+// The First matrix is :
+// 1 2
+// 3 4
 
-The second matrix is :
-5 6
-7 8
+// The second matrix is :
+// 5 6
+// 7 8
 
-The multiplication of two matrix is :
-19 22
-43 50
-*/
+// The multiplication of two matrix is :
+// 19 22
+// 43 50
