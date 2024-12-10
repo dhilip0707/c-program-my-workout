78f82fa184230e091275c583367b97abbfa8e566
dhilip0707 <dhilipkumar.dili@gmail.com>
2024-12-10 22:06:41 +0530

    multiplicarion

--- Array/21.2d-Multiplication.c
+++ Array/21.2d-Multiplication.c
@@ -34,7 +34,7 @@ void main()
             for (k = 0; k < columns; k++)
             {
                 multiplication[i][j] = multiplication[i][j] + first[i][k] * second[k][j];
-                printf("%d\n",multiplication[i][j]);
+  
             }
         }
     }
@@ -67,30 +67,29 @@ void main()
     }
 }
 
-/*Output:
-Input the rows and columns of the matrix :2 2
+// Output:
+// Input the rows and columns of the matrix :2 2 
 
-Input elements in the first matrix:
-element-[0],[0] :1
-element-[0],[1] :2
-element-[1],[0] :3
-element-[1],[1] :4
+// Input elements in the first matrix:
+// element-[0],[0] :1
+// element-[0],[1] :2
+// element-[1],[0] :3
+// element-[1],[1] :4
 
-Input elements in the second matrix:
-element-[0],[0] :5
-element-[0],[1] :6
-element-[1],[0] :7
-element-[1],[1] :8
+// Input elements in the second matrix:
+// element-[0],[0] :5
+// element-[0],[1] :6
+// element-[1],[0] :7
+// element-[1],[1] :8
 
-The First matrix is :
-1 2
-3 4
+// The First matrix is :
+// 1 2 
+// 3 4 
 
-The second matrix is :
-5 6
-7 8
+// The second matrix is :
+// 5 6 
+// 7 8 
 
-The multiplication of two matrix is :
-19 22
-43 50
-*/
+// The multiplication of two matrix is :
+// 19 22 
+// 43 50
